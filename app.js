cad872a6213b3abac3d093602e12e18439100ca8
sub0ne1 <iam0verread@gmail.com>
2023-03-31 23:53:29 -0400

    wokring: kinda

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const {App}  = require('@slack/bolt');
 require('dotenv').config();
 
+const axios  = require('axios');
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -8,19 +10,35 @@ const app = new App({
   appToken:process.env.APP_TOKEN
 });
 
-app.message("hello", async ({ command, say }) => { // Replace hello with the message
+const URL = 'http://localhost:3030/'
+
+const getData = async (query) => {
   try {
-    say("Hi! Thanks for PM'ing me!");
-  } catch (error) {
-      console.log("err")
-    console.error(error);
+    const data = await axios.post('http://localhost:3030/query', {
+      "api_key": "68e9caf7-c7ac-45e0-89b3-42d7733569d9",
+      "query": query
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    return await data.data
+  }catch(err){
+    console.log("error:" + err)
   }
-});
 
-app.event('app_mention', async ({ event, context, client, say }) => {
-  await say(`<@${event.user}> asked to do ${event.text}`);
-});
+  // console.log(data)
+  return await data.json()
+}
 
+app.event('app_mention', async ({ event, message, context, client, say }) => {
+  var botId = context.botUserId;
+  var text = event.text.replace(`<@${botId}>`, '')
+  var dataa = await getData(text);
+  console.log("dataa:" + dataa.data);
+  await say({"text": `<@${event.user}> ${dataa.data}`, "thread_ts": event.thread_ts || event.ts });
+});
 
 (async () => {
   await app.start(3000);
